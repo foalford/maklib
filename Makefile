c2c9f514d26c5396ea936470a163615c7a53b14c
Jin Chen <jin.chen@bitcoingroup.com.au>
2016-08-19 00:14:06 +1000

    Import embox/mk work and write a template function for install_remote

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
 
+#
+# This Makefile can replicate itself and install on remote
+# hosts
+
 release_name := maklib.tgz
 download_url := http://static.bitcoingroup.com.au/$(release_name)
 installdir := /usr/local/include/$(basename $(release_name))
-
-all: tempdir := $(shell mktemp -ud)
 
 all: install
 
@@ -18,11 +20,25 @@ install:
 uninstall:
 	sudo rm -r $(installdir)
 
-build:
+clean:
+	rm $(release_name)
+
+build: $(release_name)
+
+$(release_name):
 	tar czf $(release_name) $(shell ls -d */)
 
 release: build
 	aws s3 cp $(release_name) s3://static.bitcoingroup.com.au/$(release_name)
 
-.PHONY: release build all install
+-include utils/deploy-remote.mk
+-include $(call look_for_hosts_def,test/hosts/staging)
+ifdef REMOTE_HOST
+
+SOURCES := Makefile $(release_name) 
+$(eval $(call run_remote_target,$(SOURCES),install))
+
+endif
+
+.PHONY: release build all install clean
 
---
+++ utils/deploy-remote.mk
@@ -0,0 +1,71 @@
+# vim: set filetype=make:
+#
+# Run the makefile task in remote machine
+#
+# DEPLOYMENT_SOURCES := All source files
+# DEPLOY_TARGET := the target for deployment
+#
+
+ifndef __install_deploy-agent_included
+__install_deploy-agent_included := 1
+
+include maklib/libs/core/alloc.mk
+
+ENVIRONMENT ?= staging
+ifndef ENVIRONMENT
+$(error Need to have ENVIRONMENT variable defined.)
+endif
+
+# Searching for the config for remote host
+#
+# The searching paths are:
+#    $(ENVIRONMENT)/hosts
+#    hosts/$(ENVIRONMENT)
+# Usage
+#    $(call look_for_hosts_def,staging)
+#
+look_for_hosts_def = $(eval __HOST_PATHS = $(and $1,$1) $(ENVIRONMENT)/hosts hosts/$(ENVIRONMENT))\
+	$(strip $(foreach F,$(__HOST_PATHS),$(shell if [ -f $F ]; then echo $F;fi)))
+
+
+# Create dependencies for running a package remotely
+#
+# Params:
+#   1. variable referring to files need to be included for remote run
+#   2. target for run
+# Return:
+#   A separator (if any), or empty.
+
+
+define run_remote_target
+DEPLOYMENT_SOURCES = $1
+DEPLOY_TARGET=$2
+
+SSH_OPTIONS ?=
+
+__unique_id := $(call alloc,run_remote_target)
+
+__target_make_source_file := __deploy_source-$$(__unique_id)
+
+$$(__target_make_source_file) = $(shell mktemp -u).tgz
+
+__target_deploy_remote := __deploy_remote-$$(__unique_id)
+
+$$(__target_deploy_remote): SSH_LOGIN_OPTIONS = -p $$(REMOTE_PORT) $$(SSH_OPTIONS) $$(REMOTE_USER)@$$(REMOTE_HOST) 
+$$(__target_deploy_remote): SCP_LOGIN_OPTIONS = -P $$(REMOTE_PORT) $$(SSH_OPTIONS) 
+
+$$($$(__target_make_source_file)): $$(DEPLOYMENT_SOURCES)
+	export DEST=$$(basename $$($$(__target_make_source_file))); mkdir -p $$$$DEST && cp -R $$? $$$$DEST  && tar czf $$@ -C $$$$DEST .  && cd .. && rm -r $$$$DEST
+
+$$(__target_deploy_remote): $$($$(__target_make_source_file))
+	scp $$(SCP_LOGIN_OPTIONS) $$? $$(REMOTE_USER)@$$(REMOTE_HOST):$$?
+	ssh $$(SSH_LOGIN_OPTIONS) "workdir=\$$$$(mktemp -d); tar xzf $$? -C \$$$$workdir && make -C \$$$$workdir $$(DEPLOY_TARGET) && rm -r \$$$$workdir"
+
+install_remote: $$(__target_deploy_remote)
+	@echo 'done'
+
+.PHONY: $$(__target_deploy_remote) install_remote
+endef
+
+
+endif #__install_deploy-agent_included 
